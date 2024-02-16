2fc6c66216e24b88ac1ee90d9293e4cfc17d9a28
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-02-17 01:11:39 +0600

    final

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ let count = 0;
 
 for (const btn of buttons) {
   btn.addEventListener("click", function (event) {
+    //upper cart section 
     count += 1;
     setInnerText("cart-count", count);
 
+
+    // cart section
+
     const placeName = event.target.parentNode.childNodes[1].innerText;
     const cost = event.target.parentNode.childNodes[3].childNodes[1].innerText;
-
+    
+     event.target.disabled = true ;
+    
     const selectedContainer = document.getElementById("selected-place-container");
 
     const li = document.createElement('li'); 
@@ -23,14 +29,30 @@ for (const btn of buttons) {
     
     selectedContainer.appendChild(li) ;
 
-    // const totalCost = document.getElementById('total-cost').innerText ;
-    // const convertedCost = parseInt(totalCost) ;
-    const convertedCost = parseInt(document.getElementById('total-cost').innerText);
 
-    document.getElementById('total-cost').innerText = convertedCost + parseInt(cost) ;
-   
-    
-    
+    const budget = parseInt(document.getElementById('budget').innerText) ;
+
+    if(budget - parseInt(cost) <0) {
+        alert('low budget') ;
+        return ;
+    }
+
+    document.getElementById('budget').innerText = budget - parseInt(cost) ;
+
+
+
+
+
+
+
+    // total cost section 
+    setTotalCost('total-cost', cost) ;
+
+
+    // grand total section
+
+    grandTotalCost('grand-total', cost) ;
+
 
 
 });
@@ -38,4 +60,26 @@ for (const btn of buttons) {
 
 function setInnerText(elementId, value) {
   document.getElementById(elementId).innerText = value;
+}
+
+function setTotalCost(elementId, value) {
+    const convertedCost = parseInt(document.getElementById(elementId).innerText);
+
+    const sum = convertedCost + parseInt(value) ; 
+    setInnerText(elementId, sum) ;
+}
+
+function grandTotalCost(category) {
+
+   let convertedCost = parseInt(document.getElementById('total-cost').innerText);
+
+   setInnerText('grand-total', convertedCost) ;
+
+    if(category === 'bus'){
+        setInnerText('grand-total', convertedCost + 100) ;
+    }else if (category === 'train') {
+        setInnerText('grand-total', convertedCost - 200)  ;
+    }else if(category === 'flight'){
+        setInnerText('grand-total', convertedCost + 500) ; 
+    }
 }
